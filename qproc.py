f714f2b79ad6d8c56774414072cdf0ccf3fbb9bc
Drew Youngren <dcy2@columbia.edu>
2018-02-27 16:33:08 -0500

    Fix un version. Break multicol options correctly.

--- qproc.py
+++ qproc.py
@@ -6,37 +6,37 @@ import random
 
 def sticky_shuffle(choice_list, sticky_positions):
     """Shuffles list of items, leaving those marked in sticky_positions in place."""
-    n = len(choice_list)
+    num_choices = len(choice_list)
     # m = len(sticky_positions) # no. of fixed positions
 
     sticky_positions.sort()
 
     sticky_holder = [choice_list[i] for i in sticky_positions]
-    loosey_holder = [choice_list[i] for i in range(n) if i not in sticky_positions]
+    loosey_holder = [choice_list[i] for i in range(num_choices) if i not in sticky_positions]
 
     random.shuffle(loosey_holder)
-    out = ['']*n
-    for i in range(n):
+    out = ['']*num_choices
+    for i in range(num_choices):
         if i in sticky_positions:
             out[i] = sticky_holder.pop(0)
         else:
             out[i] = loosey_holder.pop(0)
     return out
 
-def qproc(fin, args, ans_key, pdir='.'):
+def qproc(filename, args, ans_key, pdir='.'):
     """
     Reads a .tex file (see mc-template.tex) shuffling choices and finding
     correct answer for answer key.
     """
     try:
-        ques = open(fin, 'r')
+        ques = open(filename, 'r')
     except IOError:
-        print("File %s not found." % (fin,))
+        print("File %s not found." % (filename,))
         sys.exit()
 
     qno = ''.join(ques.name.split('.')[:-1]).split('/')[-1]
 
-    a2z = 'ABCDEFGHIJKLMNOPQRSTUvalWXYZ'
+    A2Z = 'ABCDEFGHIJKLMNOPQRSTUvalWXYZ'
     # rr.append(qno)
 
     if args.go:
@@ -44,8 +44,14 @@ def qproc(fin, args, ans_key, pdir='.'):
     else:
         fout = sys.stdout
 
-
+    # trash comments
     ss = re.subn(r'\n\s*%.*?\n', '\n', ques.read())[0]
+
+    # look for multicols
+    if re.search(r'multicols',ss) and not re.search(r'\\columnbreak',ss):
+        colbreak = True
+    else:
+        colbreak = False
 
     ques.close()
     ## Done with the file.
@@ -65,7 +71,8 @@ def qproc(fin, args, ans_key, pdir='.'):
             if re.search(r'%[%\s]*stick', l, re.IGNORECASE):
                 st.append(k)
             ch[k] += ' % orig: '+str(k)
-        ch = sticky_shuffle(ch, st)
+        if args.vers != 'un':
+            ch = sticky_shuffle(ch, st)
         for k, l in enumerate(ch):
     #         print( l)
             if re.search(r'%[%\s]*(correct|answer)', l, re.IGNORECASE):
@@ -74,6 +81,9 @@ def qproc(fin, args, ans_key, pdir='.'):
             rr = re.search(r'%[%\s]*orig: (\d+)', l)
             if rr:
                 ans.append(int(rr.groups()[0]))
+        if colbreak:
+            print("Breaking cols in question",qno)
+            ch[2] = ch[2] + '\n\\columnbreak\n'
     else:
         print("Nope! No choices found.")
         return ans_key
@@ -88,7 +98,7 @@ def qproc(fin, args, ans_key, pdir='.'):
 
     aout = ['']*len(ans)
     for k, val in enumerate(ans):
-        aout[val] = a2z[k].upper() if (corr == k) else a2z[k].lower()
+        aout[val] = A2Z[k].upper() if (corr == k) else A2Z[k].lower()
 
     ans_key[qno] = aout
     return ans_key
